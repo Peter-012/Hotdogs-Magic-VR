2a23504d4a78377a027bcfc85153ca7d4ce75ecd
Peter-012 <151083213+Peter-012@users.noreply.github.com>
2024-04-14 07:32:28 -0400

    crates have health

--- Assets/Scripts/DuelingScene/Hitbox/CrateObject.cs
+++ Assets/Scripts/DuelingScene/Hitbox/CrateObject.cs
@@ -1,9 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using DuelingScene.Entity;
+using UnityEngine;
 
-    using System;
-    using UnityEngine;
-public class CrateObject : MonoBehaviour {
-    public void Start()
-    {
-        
+public class CrateObject : MonoBehaviour, Entity {
+    [SerializeField] private int crateHealth = 3;
+
+    public bool damageEntity(GameObject damageSource) {
+        ProjectilePlayer projectilePlayer = damageSource.GetComponent<ProjectilePlayer>();
+        ProjectileEnemy projectileEnemy = damageSource.GetComponent<ProjectileEnemy>();
+
+        if (projectilePlayer != null || projectileEnemy != null) {
+            crateHealth--;
+
+            if (crateHealth <= 0 && Game.startGame) {
+                die();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void die() {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/DuelingScene/Hitbox/Entity.cs
+++ Assets/Scripts/DuelingScene/Hitbox/Entity.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 namespace DuelingScene.Entity
 {
     public interface Entity
